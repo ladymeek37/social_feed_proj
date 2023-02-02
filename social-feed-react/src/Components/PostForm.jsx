76f3b7e42303e04ecdb230eb922785d20688c3b7
Lady Meek <ladyjmeek@gmail.com>
2023-02-01 21:03:47 -0800

    created layout for PostForm component

--- social-feed-react/src/Components/PostForm.jsx
+++ social-feed-react/src/Components/PostForm.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import { toHaveFormValues } from '@testing-library/jest-dom/dist/matchers';
+import React, { useState } from 'react';
 
 // Create a new post to go into app... takes userInput and the way to set UserInput
 // from App as props
 
 const PostForm = (props) => {
-    return ( 
-        <form>
 
+    const [name, setName] = useState('');
+    const [post, setPost] = useState('');
+
+    function onSubmit(formEvent){
+
+        const formValuesObject = {
+            name: name,
+            post: post,
+        }
+         
+        console.log(formValuesObject);
+    }
+
+    return ( 
+        <form onSubmit={onSubmit}>
+            <label>Name  </label>
+            <input type='text' value={name} onChange = {(event) => setName(event.target.value)} /> <br/>
+            <label>Post  </label>
+            <input type='text' value={post} onChange = {(event) => setPost(event.target.value)}/> <br/>
+            <button type='submit'>Create</button>
         </form>
      );
 }
